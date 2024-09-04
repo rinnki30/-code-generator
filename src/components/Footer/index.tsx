125abcbc990917575bd66654b1b96659d72e794b
工口漫画先生 <1120369372@qq.com>
2024-09-04 17:00:21 +0800

    first-commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const defaultMessage = 'cwl';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+    />
+  );
+};
+export default Footer;
